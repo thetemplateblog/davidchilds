a795b4cf3fed6ed993691489e64dcb71b5a28536
David Childs <d.childs@elsevier.com>
2024-09-16 11:04:09 -0400

    recomendations that don't work, but lots of good info

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,24 @@
-// This is all you.
+// Menu highlighting
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPath = window.location.pathname;
+    const menuItems = document.querySelectorAll('.nav-link');
+
+    menuItems.forEach(item => {
+        if (item.getAttribute('href') === currentPath) {
+            item.classList.remove('text-gray-600');
+            item.classList.add('nav-link-active');
+        }
+    });
+});
+
+// Mobile menu toggle
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuButton = document.querySelector('[data-mobile-menu-toggle]');
+    const mobileMenu = document.querySelector('[data-mobile-menu]');
+
+    if (mobileMenuButton && mobileMenu) {
+        mobileMenuButton.addEventListener('click', function() {
+            mobileMenu.classList.toggle('hidden');
+        });
+    }
+});
